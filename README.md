4b887716264bf6adf7e2243a5cfde53e64f58a7d
Tim Eagles <tim.eagles.au@gmail.com>
2023-12-03 12:51:08 -0800

    Day 2 Part 1 and 2.

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ My TypeScript submissions to https://adventofcode.com/ 2023
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -79,9 +79,9 @@ Both parts: 6.339ms
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.483ms
+Time part 2: 1.368ms
+Both parts: 2.852ms
 ```
 
 ```
@@ -246,8 +246,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 6.339ms
+Total stars: 4/50
+Total time: 9.191ms
 ```
 
 <!--/RESULTS-->
--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,35 +1,100 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const gameRegex = /Game (\d+):(.*)/g;
+const drawRegex = /\s*(\d+) (\w+),*/g;
+const parseInput = (rawInput: string) => {
+  return rawInput.split("\n").map((line) => {
+    const gameMatches = [...line.matchAll(gameRegex)];
+    const draws = gameMatches[0][2].split(";").map((draws) =>
+      [...draws.matchAll(drawRegex)].reduce(
+        (o, draw) => {
+          // console.log("color: " + draw[2]);
+          // console.log("n: " + draw[1]);
+          (o as { [index: string]: number })[draw[2]] = Number(draw[1]);
+          // console.log(o);
+          return o;
+        },
+        { red: 0, blue: 0, green: 0 },
+      ),
+    );
+    // console.log(gameMatches)
+    return {
+      gameId: Number(gameMatches[0][1]),
+      draws: draws,
+      minimumDraw: draws.reduce(
+        (o, draw) => {
+          Object.keys(o).forEach((color) => {
+            (o as { [index: string]: number })[color] = Math.max(
+              (o as { [index: string]: number })[color],
+              (draw as { [index: string]: number })[color],
+            );
+          });
+          return o;
+        },
+        { red: 0, blue: 0, green: 0 },
+      ),
+    };
+  });
+};
 
 const part1 = (rawInput: string) => {
+  const referenceMap = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
   const input = parseInput(rawInput);
+  const validIds = input.map((game) => {
+    if (
+      Object.keys(referenceMap).reduce((b, color) => {
+        return (
+          b &&
+          (game.minimumDraw as { [index: string]: number })[color] <=
+            (referenceMap as { [index: string]: number })[color]
+        );
+      }, true)
+    ) {
+      return game.gameId;
+    } else {
+      return 0;
+    }
+  });
 
-  return;
+  return validIds.reduce((acc, e) => acc + e, 0);
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  const powers = input.map((game) => {
+    return Object.keys(game.minimumDraw).reduce((acc, color) => {
+      return (game.minimumDraw as { [index: string]: number })[color] * acc;
+    }, 1);
+  });
+  return powers.reduce((acc, e) => acc + e, 0);
 };
+
+const PROVIDED_INPUT = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: PROVIDED_INPUT,
+        expected: 8,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: PROVIDED_INPUT,
+        expected: 2286,
+      },
     ],
     solution: part2,
   },

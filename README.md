18c3ca3235aac390de657184461a877452113a1e
Tim Eagles <tim.eagles.au@gmail.com>
2023-12-02 23:09:09 -0800

    Day 1 Part 2.

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ My TypeScript submissions to https://adventofcode.com/ 2023
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%86/yellow)](src/day01)
+[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 ![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
@@ -72,9 +72,9 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 608.349ms
-Time part 2: -
-Both parts: 608.349ms
+Time part 1: 1.642ms
+Time part 2: 5.314ms
+Both parts: 6.956ms
 ```
 
 ```
@@ -246,8 +246,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 1/50
-Total time: 608.349ms
+Total stars: 2/50
+Total time: 6.956ms
 ```
 
 <!--/RESULTS-->
--- src/day01/index.ts
+++ src/day01/index.ts
@@ -2,44 +2,82 @@ import run from "aocrunner";
 
 const parseInput = (rawInput: string) => rawInput;
 
+const partOneNumerator = /([\d])/g;
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
   var runningSum = 0;
   input.split("\n").forEach((line) => {
-    var firstDigit: number | null = null;
-    var lastDigit = null;
-    [...line].forEach((charAt) => {
-      var charNumber = Number(charAt);
-      // console.log("Decoded " + charNumber);
-      if (!isNaN(charNumber)) {
-        if (firstDigit == null) {
-          firstDigit = charNumber;
-          // console.log("Frist Assigned " + charNumber);
-        }
-        lastDigit = charNumber;
-        // console.log("Last Assigned " + charNumber);
-      }
-    });
-    if (firstDigit != null && lastDigit != null) {
-      var lineSum = firstDigit * 10 + lastDigit;
-      // console.log("Linesum " + lineSum);
-      runningSum += lineSum;
-    }
+    // console.log("line: " + line);
+    const matches = Array.from(line.matchAll(partOneNumerator)).map(
+      (match) => match[0],
+    );
+    // console.log("tokens: " + matches);
+    // console.log("first " + matches[0]);
+    // console.log("last " + matches[matches.length - 1]);
+    var lineSum = Number(matches[0]) * 10 + Number(matches[matches.length - 1]);
+    // console.log("Linesum " + lineSum);
+    runningSum += lineSum;
   });
   return runningSum;
 };
 
-const numerator = /([\d]|one|two|three|four|five|six|seven|eight|nine)/g;
+// const numerator = /([\d]|one|two|three|four|five|six|seven|eight|nine)/g;
+const NumberWords = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+const numerator = new RegExp(
+  "([\\d]|" + Object.keys(NumberWords).join("|") + ")",
+  "g",
+);
+const reverseNumerator = new RegExp(
+  "([\\d]|" +
+    Object.keys(NumberWords)
+      .map((s) => [...s].reverse().join(""))
+      .join("|") +
+    ")",
+  "g",
+);
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
   var runningSum = 0;
-  input.split("\n").forEach((line) => {
-    const matches = line.matchAll(numerator);
-
-    for (const match of matches) {
-      console.log(match);
-      console.log(match.index);
+  const lookUp = (str: string, reverse: boolean) => {
+    var charNumber = Number(str);
+    // console.log("Decoded " + charNumber);
+    if (isNaN(charNumber)) {
+      if (reverse) {
+        str = [...str].reverse().join("");
+      }
+      charNumber = (NumberWords as { [index: string]: number })[str];
+      // console.log("StrDecoded" + charNumber);
     }
+    return charNumber;
+  };
+  input.split("\n").forEach((line) => {
+    // console.log("line: " + line);
+    const forwardMatches = Array.from(line.matchAll(numerator)).map(
+      (match) => match[0],
+    );
+    // console.log("forward-tokens: " + forwardMatches);
+    // console.log("first " + forwardMatches[0]);
+    const rLine = [...line].reverse().join("");
+    // console.log("rline: " + rLine);
+    const reverseMatches = Array.from(rLine.matchAll(reverseNumerator)).map(
+      (match) => match[0],
+    );
+    // console.log("reverse-tokens: " + reverseMatches);
+    // console.log("last " + reverseMatches[0]);
+    var lineSum =
+      lookUp(forwardMatches[0], false) * 10 + lookUp(reverseMatches[0], true);
+    // console.log("Linesum " + lineSum);
+    runningSum += lineSum;
   });
   return runningSum;
 };
@@ -73,5 +111,5 @@ zoneight234
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });

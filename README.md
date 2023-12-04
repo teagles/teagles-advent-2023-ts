20d14b21fe03afbb16b29a84e21e68c0657c40b7
Tim Eagles <tim.eagles.au@gmail.com>
2023-12-03 22:12:32 -0800

    Day 4 Part 1 complete.

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ My TypeScript submissions to https://adventofcode.com/ 2023
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%86/yellow)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -93,9 +93,9 @@ Both parts: 10.185ms
 
 ```
 Day 04
-Time part 1: -
+Time part 1: 2.8ms
 Time part 2: -
-Both parts: -
+Both parts: 2.8ms
 ```
 
 ```
@@ -246,8 +246,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 19.376ms
+Total stars: 7/50
+Total time: 22.175ms
 ```
 
 <!--/RESULTS-->
--- src/day04/index.ts
+++ src/day04/index.ts
@@ -1,11 +1,53 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+type Card = {
+  number: number;
+  winningNumbers: Set<number>;
+  playingNumbers: Set<number>;
+};
 
+const LineMatcher = /Card\s+(\d+):([^\|]+)\|(.*)$/;
+const NumberMatcher = /(\d+)/g;
+const parseInput = (rawInput: string) => {
+  const cards = new Array<Card>();
+  rawInput.split("\n").forEach((line) => {
+    const match = line.match(LineMatcher);
+    // console.log(match);
+    if (match != null) {
+      const card: Card = {
+        number: Number(match[1]),
+        winningNumbers: new Set(
+          [...match[2].matchAll(NumberMatcher)].map((n) => Number(n[0])),
+        ),
+        playingNumbers: new Set(
+          [...match[3].matchAll(NumberMatcher)].map((n) => Number(n[0])),
+        ),
+      };
+      // if (card.winningNumbers.size != 10 || card.playingNumbers.size != 25) {
+      //   console.log("card: " + card.number);
+      //   console.log("winningNumbers: " + card.winningNumbers.size);
+      //   console.log("playingNumbers: " + card.playingNumbers.size);
+      // }
+      cards.push(card);
+    }
+  });
+  return {
+    cards: cards,
+  };
+};
+
+const score = (card: Card) => {
+  const winners = [...card.winningNumbers].filter((x) =>
+    card.playingNumbers.has(x),
+  );
+  if (winners.length > 0) {
+    return Math.pow(2, winners.length - 1);
+  }
+  return 0;
+};
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  return input.cards.map((c) => score(c)).reduce((acc, e) => acc + e, 0);
 };
 
 const part2 = (rawInput: string) => {

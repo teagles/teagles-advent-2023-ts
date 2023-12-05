5eeb27e552a7c19aac2c7f110e14bf4970f0100e
Tim Eagles <tim.eagles.au@gmail.com>
2023-12-04 21:09:47 -0800

    Day 5 Part 1 start.

---
+++ src/day05/index.ts
@@ -0,0 +1,70 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput;
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`,
+        expected: 35,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: true,
+});

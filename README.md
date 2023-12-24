030cb33c0e29c03da5cdcc97ab31cd00793299dc
Tim Eagles <tim.eagles.au@gmail.com>
2023-12-23 22:09:46 -0800

    Day 6 Part 1 and 2 complete.

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ My TypeScript submissions to https://adventofcode.com/ 2023
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -107,9 +107,9 @@ Both parts: 1383899.839ms
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.903ms
+Time part 2: 35.25ms
+Both parts: 36.153ms
 ```
 
 ```
@@ -246,8 +246,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 1385215.459ms
+Total stars: 12/50
+Total time: 1385251.611ms
 ```
 
 <!--/RESULTS-->
--- src/day06/index.ts
+++ src/day06/index.ts
@@ -1,17 +1,63 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const InputParser =
+  /Time:(?<times>(\s+\d+)+)\nDistance:(?<distances>(\s+\d+)+)/g;
+const NumberParser = /\d+/g;
+
+const parseNumbers = (unParsedNumbers: string) => {
+  return [...unParsedNumbers.matchAll(NumberParser)].map((t) => Number(t[0]));
+};
+
+type Race = {
+  time: number;
+  distance: number;
+};
+
+const parseInput = (rawInput: string) => {
+  const groups = [...(rawInput + "\n").matchAll(InputParser)][0].groups;
+  const times = parseNumbers(groups!["times"]);
+  const distances = parseNumbers(groups!["distances"]);
+  return times.map((t, i) => {
+    return { time: t, distance: distances[i] } as Race;
+  });
+};
+
+const parseInput2 = (rawInput: string) => {
+  const groups = [...(rawInput + "\n").matchAll(InputParser)][0].groups;
+  const time = Number(groups!["times"].replace(/\s/g, ""));
+  const distance = Number(groups!["distances"].replace(/\s/g, ""));
+  return { time: time, distance: distance } as Race;
+};
+
+const nWins = (race: Race) => {
+  const maxT = Math.floor(race.time / 2);
+  const dT = race.time % 2;
+  // Yeah there's almost certainly an O(1) way of doing this but O(n) (or is it O(logn)?) is still fast enough.
+  var i = 0;
+  var wins = 0;
+  while ((maxT - i) * (maxT + i + dT) > race.distance) {
+    i++;
+    wins++;
+  }
+  wins = wins * 2;
+  if (dT == 0) {
+    wins--;
+  }
+  return wins;
+};
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-
-  return;
+  // console.log(input);
+  const winsA = input.map(nWins);
+  // console.log(winsA);
+  return input.reduce((p, c) => p * nWins(c), 1);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  const input = parseInput2(rawInput);
 
-  return;
+  return nWins(input);
 };
 
 const TEST_DATA = `Time:      7  15   30
@@ -28,13 +74,13 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: TEST_DATA,
-      //   expected: "",
-      // },
+      {
+        input: TEST_DATA,
+        expected: 71503,
+      },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
